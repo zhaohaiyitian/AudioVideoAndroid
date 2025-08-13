f0d84dd316f83dc80dc8e65773bd4a552a1afd10
wangjie03 <wangjie03@zenmen.com>
2025-08-13 16:26:44 +0800

    [功能]实现分屏效果

--- app/src/main/res/raw/camera_frag.glsl
+++ app/src/main/res/raw/camera_frag.glsl
@@ -1,11 +1,20 @@
 #extension GL_OES_EGL_image_external : require
 
+// 顶点程序传过来的
 varying vec2 aCoord;
+// 采样器
 uniform samplerExternalOES vTexture; // uniform相当于java/c中的全局变量
 
 void main() {
     // 确定每个像素的最终颜色
     // 可以与纹理结合，实现纹理映射
     // 通过它可以实现各种颜色特效，如光照，高亮等
-    gl_FragColor = texture2D(vTexture, aCoord);
+    vec4 tc = texture2D(vTexture,aCoord);
+    float x = aCoord.x; // 分屏操作
+    if(x < 0.5) {
+        x+=0.25;
+    } else {
+        x-=0.25;
+    }
+    gl_FragColor = texture2D(vTexture, vec2(x,aCoord.y)); // texture2D 内置函数
 }
--- app/src/main/res/raw/camera_vert.glsl
+++ app/src/main/res/raw/camera_vert.glsl
@@ -2,19 +2,19 @@
 
 
 
-attribute vec4 vPosition;
+attribute vec4 vPosition; // 接收世界坐标
 
-attribute vec4 vCoord;
+attribute vec4 vCoord; // 接收纹理坐标
 
-varying vec2 aCoord;
+varying vec2 aCoord; // 传给片元着色器
 
-uniform mat4 vMatrix;
+uniform mat4 vMatrix; // 变换矩阵
 
 
 void main() {
     // 以gl开头的变量都是内部变量
     // 顶点着色器需要给gl_Position赋值确定顶点最终位置
     // 渲染管线根据gl_Position进行图元装配
-    gl_Position = vPosition;
+    gl_Position = vPosition; // 确定是绘制一个矩形
     aCoord = (vMatrix*vCoord).xy;
 }
